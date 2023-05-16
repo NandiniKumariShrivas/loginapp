89102908b89cb75a9d5d7db6b4a692545deb6fd3
NandiniKumariShrivas <nandini08.shrivas97@gmail.com>
2023-05-17 02:03:04 +0530

    add login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="login_box">
+      <img src="./twiter.png" alt="Twitter_logo"className="logo" />
+      <h2 >Sign In to Twitter</h2>
+      <button>
+      <img src="./google.png" alt="google_logo"className="google" />
+      Sign In with Google
+      </button>
+      <button>
+      <img src="./apple.png" alt="apple_logo"className="google" />
+      Sign In with Apple
+      </button>
+      <hr></hr>
+      <span>Or</span>
+      <form className="form">
+        <input type="text"
+        className="input"
+        placeholder="Phone, Email, Username" />
+        <button>Next</button>
+      </form>
+      <button className="forget"> Forget Password</button>
+      <p className="P">Don't Have an account<a>Sign Up</a></p>
+      
+     </div>
     </div>
   );
 }
